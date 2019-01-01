068e905afbb51a42d5017967a89a3e79fdcae500
mostafa-sakhiri <mostafa.sakhiri@gmail.com>
2019-01-02 00:34:45 +0100

    Create products components

---
+++ src/components/Products.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="products">
+    <div v-for="product in products">
+      {{product.name}} - {{product.description}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "products",
+  props: {
+    products: Array
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
