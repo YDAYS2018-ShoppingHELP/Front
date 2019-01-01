bfa9c5d6c546c34e3c684481142d64181658ef6a
mostafa-sakhiri <mostafa.sakhiri@gmail.com>
2019-01-02 00:33:05 +0100

    Create panel compoent

---
+++ src/components/Panel.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="ba br4 b--light-gray ph2 pointer flex items-center b--custom-blue hover-custom-blue" id="panel">
+		<md-icon class="notransition" style="font-size: 16px!important;">add_shopping_cart</md-icon>
+		<span class="pl">{{price}} €</span>
+	</div>
+</template>
+
+<script>
+export default {
+  name: "panel",
+  props: {
+    price: String
+  }
+};
+</script>
