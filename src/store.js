1797e6190b30df539fc2556fe98cd666b283f6ee
mostafa-sakhiri <mostafa.sakhiri@gmail.com>
2019-01-02 00:31:50 +0100

    Setup store

--- src/store.js
+++ src/store.js
@@ -4,7 +4,19 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    products: [
+      { id: 1, name: "Oeufs", description: "Le Goût Primeur Nature 17,5% MG" },
+      { id: 2, name: "Lait", description: "Le Goût Primeur Nature 17,5% MG" }
+    ],
+    filteredProducts: []
+  },
+  mutations: {
+    filterProducts(state, query) {
+      state.filteredProducts = state.products.filter(product =>
+        product.name.concat(product.description).includes(query)
+      );
+    }
+  },
   actions: {}
 });
