edaa58ad76030682951eaa16547a91d678e0c421
mostafa-sakhiri <mostafa.sakhiri@gmail.com>
2019-02-24 22:31:26 +0100

    Fix navbar icons display

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="bg-dark-gray light-gray cf pv2 flex items-center" id="navbar">
   	<span class="pl3">
-		<md-icon v-if="dashboardDisplay" style="font-size: 28px!important;">account_circle</md-icon>
-		<md-icon v-else style="font-size: 28px!important;">home</md-icon>
-	</span>
+  		<md-icon v-if="dashboardDisplay" style="font-size: 28px!important;">account_circle</md-icon>
+      <router-link v-else to="/">
+  		  <md-icon style="font-size: 28px!important;">home</md-icon>
+      </router-link>
+  	</span>
   	<span class="center"><searchProduct/></span>
   	<span class="center"><panel price=200 /></span>
   	<span class="pointer pr3 hover-custom-blue">
