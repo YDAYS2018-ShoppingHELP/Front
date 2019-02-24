112099d5d788c6ec45876aaed0335a44144efcf6
mostafa-sakhiri <mostafa.sakhiri@gmail.com>
2019-02-24 22:34:48 +0100

    Refacto router about page import

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import About from "./views/About.vue";
+import Dashboard from "./views/Dashboard.vue";
 
 Vue.use(Router);
 
@@ -19,8 +21,15 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      component: About
+    },
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: Dashboard
     }
   ]
 });
