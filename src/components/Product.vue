e8d410ffee99c29de81e4c7edbe9c307aa87b31c
mostafa-sakhiri <mostafa.sakhiri@gmail.com>
2019-04-05 00:15:02 +0200

    Add reduce && increase product qty

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -4,19 +4,35 @@
   	<div class="w-60 ph3 tl">
 	    <div class="fw7 primary-color">{{product.name}}</div>
 	    <span class="dib">{{product.description}}</span>
-	</div>
-	<div class="absolute bottom-0 right-0 w-20 pv1 mb2 bg-primary-color white tc br4">
-		<md-icon class="notransition" style="font-size: 16px!important;">add_shopping_cart</md-icon>
-		13 €
-	</div>
+	  </div>
+
+    <div v-if="product.existsInPanel" class="absolute bottom-0 right-0 w-20 mb2 white tc br4 pointer">
+      <span class="dib w1 ph1 bg-primary-color ba b--primary-color addon-left" @click="reduceProductQty(product)">-</span>
+      <span class="dib ph3 ba b--light-gray black">{{product.qty}}</span>
+      <span class="dib w1 ph1 bg-primary-color ba b--primary-color addon-right" @click="increaseProductQty(product)">+</span>
+    </div>
+	  <div v-else class="absolute bottom-0 right-0 w-20 pv1 mb2 bg-primary-color white tc br4 pointer" @click="addProductToPanel(product);">
+  		<md-icon class="notransition" style="font-size: 16px!important;">add_shopping_cart</md-icon>
+  		{{product.price}} €
+  	</div>
   </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
   name: "product",
   props: {
     product: Object
+  },
+
+  methods: {
+    ...mapActions([
+      "addProductToPanel",
+      "reduceProductQty",
+      "increaseProductQty"
+    ])
   }
 };
 </script>
@@ -24,5 +40,15 @@ export default {
 <style>
 .product {
   height: 150px;
+}
+
+.addon-left {
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+}
+
+.addon-right {
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -6,19 +6,71 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     products: [
-      { id: 1, name: "Oeufs", description: "Le Goût Primeur Nature 17,5% MG" },
-      { id: 2, name: "Lait", description: "Le Goût Primeur Nature 17,5% MG." },
-      { id: 3, name: "Lait", description: "Le Goût Primeur Nature 17,5% MG" },
-      { id: 4, name: "Lait", description: "Le Goût Primeur Nature 17,5% MG" }
+      {
+        _id: 1,
+        name: "Oeufs",
+        description: "Le Goût Primeur Nature 17,5% MG",
+        price: 13,
+        existsInPanel: false
+      },
+      {
+        _id: 2,
+        name: "Lait",
+        description: "Le Goût Primeur Nature 17,5% MG.",
+        price: 13,
+        existsInPanel: false
+      },
+      {
+        _id: 3,
+        name: "Lait",
+        description: "Le Goût Primeur Nature 17,5% MG",
+        price: 13,
+        existsInPanel: false
+      }
     ],
-    filteredProducts: []
+    filteredProducts: [],
+    panel: []
   },
   mutations: {
     filterProducts(state, query) {
       state.filteredProducts = state.products.filter(product =>
         product.name.concat(product.description).includes(query)
       );
+    },
+
+    addProductToPanel(state, product) {
+      if (!state.panel.find(({ _id }) => _id === product._id)) {
+        product.existsInPanel = true;
+        product.qty = 1;
+        state.panel.push(product);
+      }
+    },
+
+    reduceProductQty(state, product) {
+      let foundedProduct = state.panel.find(({ _id }) => _id === product._id);
+      if (foundedProduct && product.qty > 0) {
+        foundedProduct.qty -= 1;
+      }
+    },
+
+    increaseProductQty(state, product) {
+      let foundedProduct = state.panel.find(({ _id }) => _id === product._id);
+      if (foundedProduct) {
+        foundedProduct.qty += 1;
+      }
     }
   },
-  actions: {}
+  actions: {
+    addProductToPanel({ commit }, product) {
+      commit("addProductToPanel", product);
+    },
+
+    reduceProductQty({ commit }, product) {
+      commit("reduceProductQty", product);
+    },
+
+    increaseProductQty({ commit }, product) {
+      commit("increaseProductQty", product);
+    }
+  }
 });
