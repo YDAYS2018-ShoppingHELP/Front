34ca723036af3ca16572a6fbbcf91f2eb2ad123b
mostafa-sakhiri <mostafa.sakhiri@gmail.com>
2019-04-05 00:13:50 +0200

    Create panel page

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from "vue-router";
 import Home from "./views/Home.vue";
 import About from "./views/About.vue";
 import Dashboard from "./views/Dashboard.vue";
+import Panel from "./views/Panel.vue";
 
 Vue.use(Router);
 
@@ -18,18 +19,17 @@ export default new Router({
     {
       path: "/about",
       name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: About
     },
     {
       path: "/dashboard",
       name: "dashboard",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: Dashboard
+    },
+    {
+      path: "/panel",
+      name: "panel",
+      component: Panel
     }
   ]
 });
---
+++ src/views/Panel.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+    <div v-if="panel.length > 0">
+		  <Products :products="panel"/>
+	  </div>
+    <div v-else class="mt5 dark-gray">
+      Votre panier est vide!
+    </div>
+  </div>
+</template>
+
+<script>
+// import modules
+import { mapState } from "vuex";
+
+// import components
+import Products from "@/components/Products";
+
+export default {
+  name: "panel",
+  components: {
+    Products
+  },
+  computed: {
+    ...mapState(["panel"])
+  }
+};
+</script>
